ddb00e4da5c1aa2acf6c9ac83286c58e7797a602
Yuki Omori <yukiomori4211@gmail.com>
2017-11-15 12:31:59 +0900

    チャットルームとuserIdの入力受付の仕様に変更。

--- script/main.js
+++ script/main.js
@@ -1,5 +1,5 @@
-// fire baseとの接続?
-console.log('読み込み開始');
+
+
 let config = {
     apiKey: "AIzaSyAiipaC7uoKV9LXiJ1XXHwAkJHv89BQbP0",
     authDomain: "chatapp-783ff.firebaseapp.com",
@@ -11,35 +11,41 @@ let config = {
 firebase.initializeApp(config);
 
 let newPostRef = firebase.database();
+let userId = prompt('あなたのIDを入力してください');
+
+// idの重複があれば、表示
+let roomId = prompt('チャットルームIDを入力してください');
 // set buttonが押されたときの処理
 
-$('#set').on('click', () => {
-  let userId = $('#userName').val();
-  $('#send').on('click', () => {
-    let d = new Date();
-    let h = d.getHours().length < 2 ? 0 + d.getHours() : d.getHours();
-    let m = d.getMinutes().length < 2 ? 0 + d.getMinutes() : d.getMinutes();
-    let s = d.getSeconds().length < 2 ? 0 + d.getSeconds() : d.getSeconds();
-    let date = h + ':' + m + ':' + s;
+function returnDigitalTime () {
+  let d = new Date();
+  let h = d.getHours() < 10 ? '0' + d.getHours() : d.getHours();
+  let m = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
+  let s = d.getSeconds() < 10 ? '0' + d.getSeconds() : d.getSeconds();
+  return h + ':' + m + ':' + s;
+}
 
-    newPostRef.ref(`user/${userId}/${date}`).set({
-      userName: $('#userName').val(),
-      text: $('#text').val(),
-      date: date
-    });
+$('#send').on('click', () => {
+  let date = returnDigitalTime();
 
-    $('#text').val('');
-
-    newPostRef.ref(`user/${userId}`).on('child_added', function(data) {
-      let content = data.val();
-      let addedStr = '<dl id="' + content.date + '"><dt>' + content.userName + '</dt><dd>' + content.text + '</dd><dd>' + content.date + '</dd><dl>';
-      $('#history').append(addedStr);
-      $('#history').animate({scrollTop: $('#history')[0].scrollHeight}, 'fast');
-    });
+  newPostRef.ref(`${roomId}/post/${date}`).set({
+    userName: userId,
+    text: $('#text').val(),
+    date: date
   });
+
+  $('#text').val('');
 });
-  // userIDの取得
-  // submit が押されたとき
-    // fire baseにdate/userName/textを格納
-    // テキストエリアをクリア
-    // prepend childを取得
+
+$('#text').on('keydown', (e) => {
+  if (e.keyCode === 13) {
+    
+  }
+});
+
+newPostRef.ref(`${roomId}/post`).on('child_added', function(data) {
+  let content = data.val();
+  let addedStr = '<dl id="' + content.date + '"><dt>' + content.userName + '</dt><dd>' + content.text + '</dd><dd>' + content.date + '</dd><dl>';
+  $('#history').append(addedStr);
+  $('#history').animate({scrollTop: $('#history')[0].scrollHeight}, 'fast');
+});
